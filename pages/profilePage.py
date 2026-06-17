e3e78ccaf84cced0649854f6ac591debaaf44b32
neils <jeebanneils@gmail.com>
2026-06-17 18:43:55 +0400

    Got rid of the counter, as there was a bug where users would have the same profile picture. So instead of using counters, I used the names of the users as it felt better and if they change the profile picture; it just overwrites the same file rather than making another counter file

--- pages/profilePage.py
+++ pages/profilePage.py
@@ -4,19 +4,14 @@ import os
 
 if not st.session_state.get("isUserLoggedIn"):
     st.warning("Please login first")
-else:
+else: # This part below will only run if user is logged in
     uploadedFile = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
     if uploadedFile:
-        counter = 0
-        for i in os.listdir("pages/images"):
-            counter += 1
         with open(os.path.join("pages/images", uploadedFile.name), "wb") as f:
             f.write(uploadedFile.getvalue())
-            os.rename(os.path.join("pages/images", uploadedFile.name), os.path.join("pages/images",f"{counter}.png"))
+            os.rename(os.path.join("pages/images", uploadedFile.name), os.path.join("pages/images",f"{st.session_state.get("username")}.png"))
         with sql.connect("project_data.db") as connection:
             cursor = connection.cursor()
             cursor.execute("SELECT profilePicturePath from users WHERE username = ?", (st.session_state.username,))
             currentProfilePicture = cursor.fetchone()[0]
-            if currentProfilePicture != "defaultProfile.jpg":
-                os.remove(os.path.join("pages/images", currentProfilePicture))
-            cursor.execute(f"UPDATE users SET profilePicturePath = ? WHERE username = ?", (f"{counter}.png",st.session_state.get("username"),))
+            cursor.execute(f"UPDATE users SET profilePicturePath = ? WHERE username = ?", (f"{st.session_state.get("username")}.png",st.session_state.get("username"),))
