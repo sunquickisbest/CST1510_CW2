8ee6b53148caffeed7db17e2e708571a7252cb16
neils <jeebanneils@gmail.com>
2026-07-19 18:02:52 +0400

    Fixed db bug

--- app_model/db.py
+++ app_model/db.py
@@ -4,6 +4,6 @@ import pandas as pd
 def CSVToDB(x, name):
     pd.read_csv(x).to_sql(name, con=sql.connect("../DATA/project_data.db"))
 
-CSVToDB("DATA/cyber_incidents.csv", "CyberIncidents")
-CSVToDB("DATA/datasets_metadata.csv", "Metadata")
-CSVToDB("DATA/it_tickets.csv", "IT_Tickets")
+CSVToDB("../DATA/cyber_incidents.csv", "CyberIncidents")
+CSVToDB("../DATA/datasets_metadata.csv", "Metadata")
+CSVToDB("../DATA/it_tickets.csv", "IT_Tickets")
